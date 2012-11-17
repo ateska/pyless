ee442b263586b94263514857cad9311a9f6ba6a7
ateska <ales.teska@gmail.com>
2012-11-17 23:42:47 +0100

    Adding Bootstrap CSS to unit tests.

---
+++ tests/bootstrap/readme.txt
@@ -0,0 +1,2 @@
+This folder contains distribution version of Bootstrap CSS used for testing our CSS parser abilities.
+For more details about Bootstrap CSS visit http://twitter.github.com/bootstrap
--- tests/test_file.py
+++ tests/test_file.py
@@ -25,18 +25,18 @@ TestCSS.AddTestDirGlob(os.path.join(os.path.dirname(__file__),'..','tests','css'
 
 #
 
-# class TestBootstrapCSS(FileBasedParserTest):
-# 	'''Iterate via test/bootstrap/ folder, seek for *.css files and parse them.
-# 	There should be no parsing error.
-# 	'''
+class TestBootstrapCSS(FileBasedParserTest):
+	'''Iterate via test/bootstrap/ folder, seek for *.css files and parse them.
+	There should be no parsing error.
+	'''
 
-# 	counter = 0
-# 	name = 'bootstrap'
+	counter = 0
+	name = 'bootstrap'
 
-# 	def test_count(self):
-# 		'Ensuring that there is correct number of CSS files to test parser.'
-# 		self.assertEqual(self.counter, 4, "Number of *.css files to be tested is not correct (counter={0})".format(self.counter))
+	def test_count(self):
+		'Ensuring that there is correct number of CSS files to test parser.'
+		self.assertEqual(self.counter, 4, "Number of *.css files to be tested is not correct (counter={0})".format(self.counter))
 
-# TestBootstrapCSS.AddTestDirGlob(os.path.join(os.path.dirname(__file__),'..','tests','bootstrap','*.css'))
+TestBootstrapCSS.AddTestDirGlob(os.path.join(os.path.dirname(__file__),'..','tests','bootstrap','*.css'))
 
 #
