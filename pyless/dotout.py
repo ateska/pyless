af619aee77d09a32ca83e860246a42c614dc6b25
ateska <ales.teska@gmail.com>
2012-11-20 23:30:13 +0100

    Extracting .dot save functionality into dedicated file.

---
+++ pyless/dotout.py
@@ -0,0 +1,41 @@
+import os, cgi
+from .lesscssParser import tokenNames
+
+def save_result(parser_result, fname):
+	'''Save parser result (tree) to .dot file - this can be then open by Graphviz and investigated.'''
+
+	fout = open(fname, "w")
+	fout.write('digraph {0} {{\nratio = fill;packMode=graph;\n'.format(os.path.basename(fname)[:-4].replace('-','_')))
+	fout.write('node [style="filled",fontname=Arial];\n')
+
+	def twalker(t):
+		tname = tokenNames[t.getType()]
+		if tname == str(t):
+			fout.write('"{0}" [shape=ellipse,label="{1}",fillcolor=paleturquoise];\n'.format(
+				id(t),
+				_escape_dot_label(t)
+				)
+			)
+		else:
+			fout.write('"{0}" [shape=record,label="{{{2}|{1}}}",fillcolor=palegreen];\n'.format(
+				id(t),
+				_escape_dot_label(t),
+				_escape_dot_label(tname)
+				)
+			)
+
+		if t.parent is not None:
+			fout.write('"{0}" -> "{1}";\n'.format(id(t.parent), id(t)))
+
+		for ch in t.children:
+			twalker(ch)
+
+	twalker(parser_result.tree)
+
+	fout.write("}\n")
+
+
+_label_repls = {'\n' : '\\n', '{' : '\{', '}': '\}'}
+def _escape_dot_label(label):
+	label = cgi.escape("{0}".format(label), True)
+	return reduce(lambda a, kv: a.replace(*kv), _label_repls.iteritems(), label)
--- tests/_testbed.py
+++ tests/_testbed.py
@@ -1,8 +1,6 @@
-import unittest, glob, os, cgi
+import unittest, glob, os
 import pyless
 from pyless.antlr3 import tree
-from pyless.lesscssParser import tokenNames
-
 ##
 
 def _fbtgen(fname):
@@ -25,44 +23,6 @@ class FileBasedParserTest(unittest.TestCase):
 
 		r = result.tree.toStringTree()
 		self.assertIsNotNone(r, "Parsing silently failed!")
-
-		###
-
-		fout = open(fname+'.dot', "w")
-		fout.write('digraph {0} {{\nratio = fill;packMode=graph;\n'.format(os.path.basename(fname)[:-4].replace('-','_')))
-		fout.write('node [style="filled",fontname=Arial];\n')
-		def twalker(t):
-			tname = tokenNames[t.getType()]
-			if tname == str(t):
-				fout.write('"{0}" [shape=ellipse,label="{1}",fillcolor=paleturquoise];\n'.format(
-					id(t),
-					self._escape_dot_label(t)
-					)
-				)
-			else:
-				fout.write('"{0}" [shape=record,label="{{{2}|{1}}}",fillcolor=palegreen];\n'.format(
-					id(t),
-					self._escape_dot_label(t),
-					self._escape_dot_label(tname)
-					)
-				)
-
-			if t.parent is not None:
-				fout.write('"{0}" -> "{1}";\n'.format(id(t.parent), id(t)))
-
-			for ch in t.children:
-				twalker(ch)
-
-		twalker(result.tree)
-
-		fout.write("}\n")
-	
-	
-	_label_repls = {'\n' : '\\n', '{' : '\{', '}': '\}'}
-	@classmethod
-	def _escape_dot_label(cls, label):
-		label = cgi.escape("{0}".format(label), True)
-		return reduce(lambda a, kv: a.replace(*kv), cls._label_repls.iteritems(), label)
 
 
 	@classmethod
