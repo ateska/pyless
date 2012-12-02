3fb7b6e29193516f427ce25e95a2c7993a4f65f7
ateska <ales.teska@gmail.com>
2012-12-02 20:17:20 +0100

    pyless.parse function now accepts also file-like object (on top of file name).

--- pyless/_parser.py
+++ pyless/_parser.py
@@ -1,8 +1,14 @@
 from . import antlr3
 from ._antlrwrap import lesscsslexer, lesscssparser
 
-def parse(fname):
-	char_stream = antlr3.ANTLRFileStream(fname)
+def parse(input):
+	'''
+	@param input: filename or file-like object with CSS/LESS to parse
+	'''
+	if hasattr(input, 'read'):
+		char_stream = antlr3.ANTLRStringStream(input.read())
+	else:
+		char_stream = antlr3.ANTLRFileStream(input)
 	lexer = lesscsslexer(char_stream)
 	tokens = antlr3.CommonTokenStream(lexer)
 	parser = lesscssparser(tokens)
